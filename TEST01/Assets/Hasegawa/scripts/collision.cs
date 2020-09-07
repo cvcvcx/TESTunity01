d4bb4deee2f3ecdbc5974d8399ec9794a6dc04ec
cvcvcx <cvcvcx@naver.com>
2020-09-07 16:13:16 +0900

    yukioz
    yukioz file

---
+++ TEST01/Assets/Hasegawa/scripts/collision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    public AudioClip sound;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Cola Can")
+        {
+            Debug.Log("hit!");
+            AudioSource.PlayClipAtPoint(sound, transform.position);
+            Destroy(collision.gameObject);
+        }
+            
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
